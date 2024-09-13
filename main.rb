1b88d3b08b396e115df30c3eb80d0aad830d9dff
Ren Ant <eurekavixen@gmail.com>
2024-09-13 10:41:26 +0800

    modified array of hashes: national id from bool to num

--- main.rb
+++ main.rb
@@ -1,26 +1,30 @@
 people = [
-  {name: 'Ren', age: 26, national_id: true},
-  {name: 'Josh', age: 19, national_id: false},
-  {name: 'Rose', age: 27, national_id: true},
-  {name: 'Chase', age: 27, national_id: true},
-  {name: 'Precious', age: 23, national_id: false},
-  {name: 'Lyra', age: 26, national_id: true},
-  {name: 'Lloyd', age: 23, national_id: true},
-  {name: 'Jerome', age: 25, national_id: false},
-  {name: 'Jaarl', age: 22, national_id: false},
-  {name: 'Bernard', age: 29, national_id: true},
-  {name: 'Rex', age: 24, national_id: true},
-  {name: 'Mark', age: 29, national_id: true},
-  {name: 'Jati', age: 24, national_id: false},
-  {name: 'Carl', age: 33, national_id: true},
-  {name: 'John', age: 54, national_id: true},
-  {name: 'James', age: 60, national_id: true},
-  {name: 'Jack', age: 89, national_id: false},
-  {name: 'Rylle', age: 26, national_id: true},
-  {name: 'Mary', age: 25, national_id: false},
-  {name: 'Lincoln', age: 200, national_id: false},
+  {name: 'Ren', age: 26, national_id: 1},
+  {name: 'Josh', age: 19, national_id: 2},
+  {name: 'Rose', age: 27, national_id: 3},
+  {name: 'Chase', age: 27, national_id: 4},
+  {name: 'Precious', age: 23, national_id: 5},
+  {name: 'Lyra', age: 26, national_id: 6},
+  {name: 'Lloyd', age: 23, national_id: 7},
+  {name: 'Jerome', age: 25, national_id: 8},
+  {name: 'Jaarl', age: 22, national_id: 9},
+  {name: 'Bernard', age: 29, national_id: 10},
+  {name: 'Rex', age: 24, national_id: 11},
+  {name: 'Mark', age: 29, national_id: 12},
+  {name: 'Jati', age: 24, national_id: 13},
+  {name: 'Carl', age: 33, national_id: 14},
+  {name: 'John', age: 54, national_id: 15},
+  {name: 'James', age: 60, national_id: 16},
+  {name: 'Jack', age: 89, national_id: 17},
+  {name: 'Rylle', age: 26, national_id: 18},
+  {name: 'Mary', age: 25, national_id: 19},
+  {name: 'Lincoln', age: 200, national_id: 20},
 ]
 
 people.each do |person|
   p person
 end
+
+
+
+
