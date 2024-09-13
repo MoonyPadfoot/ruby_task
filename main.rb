78456a8de4771aba00ab20f4ae9a4a07e4715777
Ren Ant <eurekavixen@gmail.com>
2024-09-13 10:26:17 +0800

    array of people(modified)

--- main.rb
+++ main.rb
@@ -1,1 +1,26 @@
-puts 'Hello World'
+people = [
+  {name: 'Ren', age: 26, national_id: true},
+  {name: 'Josh', age: 19, national_id: false},
+  {name: 'Rose', age: 27, national_id: true},
+  {name: 'Chase', age: 27, national_id: true},
+  {name: 'Precious', age: 23, national_id: false},
+  {name: 'Lyra', age: 26, national_id: true},
+  {name: 'Lloyd', age: 23, national_id: true},
+  {name: 'Jerome', age: 25, national_id: false},
+  {name: 'Jaarl', age: 22, national_id: false},
+  {name: 'Bernard', age: 29, national_id: true},
+  {name: 'Rex', age: 24, national_id: true},
+  {name: 'Mark', age: 29, national_id: true},
+  {name: 'Jati', age: 24, national_id: false},
+  {name: 'Carl', age: 33, national_id: true},
+  {name: 'John', age: 54, national_id: true},
+  {name: 'James', age: 60, national_id: true},
+  {name: 'Jack', age: 89, national_id: false},
+  {name: 'Rylle', age: 26, national_id: true},
+  {name: 'Mary', age: 25, national_id: false},
+  {name: 'Lincoln', age: 200, national_id: false},
+]
+
+people.each do |person|
+  p person
+end
